86d2b51f18a1b7beb49055374b7078f69410e93d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-21 23:54:48 +0200

    test: fix test

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from pandas import DataFrame as Df
 from pandas import read_csv
+from pandas import to_datetime
 from pandas.testing import assert_frame_equal
 
 from src import compare as m_compare
@@ -19,10 +20,6 @@ class Test_get_df_combine_files(unittest.TestCase):
         result = m_compare._get_df_combine_files(config)
         result_as_csv_export = m_compare._CsvExporter()._get_df_to_export(result).reset_index()
         expected_result = self._get_df_from_csv_expected_result()
-        print(">>> EXPECTED RESULT")
-        print(expected_result)
-        print(">>> RESULT_AS_CSV_EXPORT")
-        print(result_as_csv_export)
         assert_frame_equal(expected_result, result_as_csv_export)
 
     def _get_config_for_the_test(self) -> Config:
@@ -33,10 +30,14 @@ class Test_get_df_combine_files(unittest.TestCase):
 
     def _get_df_from_csv_expected_result(self) -> Df:
         expected_result_file_path = self.current_path.joinpath("expected_result_compare.csv")
-        return read_csv(expected_result_file_path).astype(
+        result = read_csv(expected_result_file_path).astype(
             {
                 "aws_account_1_pro_size": "Int64",
                 "aws_account_2_release_size": "Int64",
                 "aws_account_3_dev_size": "Int64",
             }
         )
+        # https://stackoverflow.com/a/26763793
+        date_column_names = ["aws_account_1_pro_date", "aws_account_2_release_date", "aws_account_3_dev_date"]
+        result[date_column_names] = result[date_column_names].apply(to_datetime)
+        return result
