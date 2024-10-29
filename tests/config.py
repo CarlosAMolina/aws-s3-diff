8f8f9d56f32598da2e6498679270753d8572865a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-10-29 22:49:16 +0100

    test: fix test

--- tests/config.py
+++ tests/config.py
@@ -7,6 +7,6 @@ from src.constants import FOLDER_NAME_S3_RESULTS
 
 def get_config_for_the_test() -> Config:
     current_path = Path(__file__).parent.absolute()
-    directory_s3_results_path = current_path.parent.joinpath(FOLDER_NAME_S3_RESULTS)
+    directory_s3_results_path = current_path.joinpath(FOLDER_NAME_S3_RESULTS)
     file_what_to_analyze_path = current_path.joinpath(FILE_NAME_S3_URIS)
     return Config(directory_s3_results_path, file_what_to_analyze_path)
