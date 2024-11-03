85f142086cdbd1b11e598e8f9f9ba7f414529077
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-03 01:02:35 +0100

    test(s3): refactor use namedtuple

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,5 +1,6 @@
 import datetime
 import unittest
+from collections import namedtuple
 
 from dateutil.tz import tzutc
 from moto import mock_aws
@@ -7,6 +8,8 @@ from moto import mock_aws
 from src import s3 as m_s3
 from tests.aws import S3
 from tests.aws import set_aws_credentials
+
+ExpectedResult = namedtuple("ExpectedResult", "file_name file_size")
 
 
 class TestS3Client(unittest.TestCase):
@@ -22,30 +25,28 @@ class TestS3Client(unittest.TestCase):
         self.mock_aws.stop()
 
     def test_get_s3_data_returns_expected_result_for_bucket_cars(self):
-        file_name = "cars-20241014.csv"
-        file_size = 49
+        expected_result = ExpectedResult("cars-20241014.csv", 49)
         s3_query = m_s3.S3Query("cars", "europe/spain/")
-        self._test_get_s3_data_returns_expected_result(file_name, file_size, s3_query)
+        self._test_get_s3_data_returns_expected_result(expected_result, s3_query)
 
     def test_get_s3_data_returns_expected_result_for_bucket_pets(self):
-        file_name = "dogs-20241019.csv"
-        file_size = 20
+        expected_result = ExpectedResult("dogs-20241019.csv", 20)
         s3_query = m_s3.S3Query("pets", "dogs/big_size/")
-        self._test_get_s3_data_returns_expected_result(file_name, file_size, s3_query)
+        self._test_get_s3_data_returns_expected_result(expected_result, s3_query)
 
-    def _test_get_s3_data_returns_expected_result(self, file_name: str, file_size: int, s3_query: m_s3.S3Query):
+    def _test_get_s3_data_returns_expected_result(self, expected_result: ExpectedResult, s3_query: m_s3.S3Query):
         s3_client = m_s3.S3Client()
         s3_data = s3_client.get_s3_data(s3_query)
         result = s3_data[0]
-        self._test_get_s3_data_returns_expected_result_for_file_name(file_name, file_size, result)
+        self._test_get_s3_data_returns_expected_result_for_file_name(expected_result, result)
 
     def _test_get_s3_data_returns_expected_result_for_file_name(
-        self, file_name: str, file_size: int, result_to_check: dict
+        self, expected_result: ExpectedResult, result_to_check: dict
     ):
         for key, expected_value in {
-            "name": file_name,
+            "name": expected_result.file_name,
             "date": datetime.datetime.now(tzutc()),
-            "size": file_size,
+            "size": expected_result.file_size,
         }.items():
             if key == "date":
                 self.assertEqual(expected_value.date(), result_to_check[key].date())
