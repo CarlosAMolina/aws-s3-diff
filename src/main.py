f217bc2ccab2d2830cce0d8dc219a39b6e368343
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-19 06:27:42 +0100

    feat(user input): improve question

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ class _IteractiveMenu:
     def _exit_program_if_no_aws_credentials_in_terminal(self):
         print("Have you generated in you terminal the AWS credentials to connect with that AWS account?")
         while True:
-            user_input = input("Y/n").lower()
+            user_input = input("Y/n: ").lower()
             if user_input == "n":
                 print("Generate the credentials to work with that AWS account and run the program again")
                 sys.exit()
