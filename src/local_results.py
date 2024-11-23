733cfea89588100f2907a7f84d5823183c1bcc0a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-23 03:28:57 +0100

    refactor(local results): extract code to file

---
+++ src/local_results.py
@@ -0,0 +1,60 @@
+import datetime
+import os
+from pathlib import Path
+
+from constants import FOLDER_NAME_S3_RESULTS
+
+
+class LocalResults:
+    # TODO remove this file when the 3º account has been analyzed.
+    _FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME = "/tmp/aws_s3_diff_analysis_date_time.txt"
+
+    def get_aws_account_index_to_analyze(self) -> int:
+        if self._get_number_of_aws_accounts_analyzed() > 2:
+            raise ValueError("All AWS accounts have been analyzed")
+        return self._get_number_of_aws_accounts_analyzed()
+
+    def create_analysis_results_folder_if_required(self, number_of_aws_accounts_to_analyze: int):
+        if (
+            self._get_number_of_aws_accounts_analyzed() == 0
+            and not self._get_path_analysis_results().is_dir()
+            or self._get_number_of_aws_accounts_analyzed() == number_of_aws_accounts_to_analyze
+        ):
+            self._create_analysis_results_folder()
+
+    def create_aws_account_results_folder(self, aws_account: str):
+        self._get_path_aws_account_analysis_results(aws_account).mkdir()
+
+    def _get_path_aws_account_analysis_results(self, aws_account: str):
+        return self._get_path_analysis_results().joinpath(aws_account)
+
+    def _create_analysis_results_folder(self):
+        print(f"Creating the results folder: {self._get_path_analysis_results()}")
+        self._get_path_analysis_results().mkdir()
+
+    def _get_number_of_aws_accounts_analyzed(self) -> int:
+        return len(self._get_aws_accounts_analyzed())
+
+    def _get_aws_accounts_analyzed(self) -> list[str]:
+        if self._get_path_analysis_results().is_dir():
+            # TODO use self._get_path_analysis_results().iterdir():
+            return os.listdir(self._get_path_analysis_results())
+        return []
+
+    def _get_path_analysis_results(self) -> Path:
+        current_path = Path(__file__).parent.absolute()
+        return current_path.parent.joinpath(FOLDER_NAME_S3_RESULTS, self._get_analysis_date_time_str())
+
+    def _get_analysis_date_time_str(self) -> str:
+        if not Path(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME).is_file():
+            self._export_date_time_str()
+        return self._get_date_time_str_stored()
+
+    def _export_date_time_str(self):
+        date_time_str = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        with open(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME, "w") as file:
+            file.write(date_time_str)
+
+    def _get_date_time_str_stored(self) -> str:
+        with open(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME) as file:
+            return file.read()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-import datetime
-import os
 import sys
-from pathlib import Path
 
 from config import get_s3_uris_file_reader
-from constants import FOLDER_NAME_S3_RESULTS
+from local_results import LocalResults
 
 
 def run():
@@ -14,7 +11,7 @@ def run():
 class _IteractiveMenu:
     def __init__(self):
         self._s3_uris_file_reader = get_s3_uris_file_reader()
-        self._local_results = _LocalResults()
+        self._local_results = LocalResults()
 
     def run(self):
         print("Welcome to the AWS S3 Diff tool!")
@@ -53,67 +50,13 @@ class _IteractiveMenu:
 class _AccountAnalyzer:
     def __init__(self, aws_account: str):
         self._aws_account = aws_account
-        self._local_results = _LocalResults()
+        self._local_results = LocalResults()
 
     def run(self):
         print(f"Analyzing the account: {self._aws_account}")
         # TODO create the folder after retrieve aws results to avoid not use the folder if any aws error
         self._local_results.create_aws_account_results_folder(self._aws_account)
-
-
-class _LocalResults:
-    # TODO remove this file when the 3º account has been analyzed.
-    _FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME = "/tmp/aws_s3_diff_analysis_date_time.txt"
-
-    def get_aws_account_index_to_analyze(self) -> int:
-        if self._get_number_of_aws_accounts_analyzed() > 2:
-            raise ValueError("All AWS accounts have been analyzed")
-        return self._get_number_of_aws_accounts_analyzed()
-
-    def create_analysis_results_folder_if_required(self, number_of_aws_accounts_to_analyze: int):
-        if (
-            self._get_number_of_aws_accounts_analyzed() == 0
-            and not self._get_path_analysis_results().is_dir()
-            or self._get_number_of_aws_accounts_analyzed() == number_of_aws_accounts_to_analyze
-        ):
-            self._create_analysis_results_folder()
-
-    def create_aws_account_results_folder(self, aws_account: str):
-        self._get_path_aws_account_analysis_results(aws_account).mkdir()
-
-    def _get_path_aws_account_analysis_results(self, aws_account: str):
-        return self._get_path_analysis_results().joinpath(aws_account)
-
-    def _create_analysis_results_folder(self):
-        print(f"Creating the results folder: {self._get_path_analysis_results()}")
-        self._get_path_analysis_results().mkdir()
-
-    def _get_number_of_aws_accounts_analyzed(self) -> int:
-        return len(self._get_aws_accounts_analyzed())
-
-    def _get_aws_accounts_analyzed(self) -> list[str]:
-        if self._get_path_analysis_results().is_dir():
-            # TODO use self._get_path_analysis_results().iterdir():
-            return os.listdir(self._get_path_analysis_results())
-        return []
-
-    def _get_path_analysis_results(self) -> Path:
-        current_path = Path(__file__).parent.absolute()
-        return current_path.parent.joinpath(FOLDER_NAME_S3_RESULTS, self._get_analysis_date_time_str())
-
-    def _get_analysis_date_time_str(self) -> str:
-        if not Path(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME).is_file():
-            self._export_date_time_str()
-        return self._get_date_time_str_stored()
-
-    def _export_date_time_str(self):
-        date_time_str = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        with open(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME, "w") as file:
-            file.write(date_time_str)
-
-    def _get_date_time_str_stored(self) -> str:
-        with open(self._FILE_PATH_NAME_ACCOUNTS_ANALYSIS_DATE_TIME) as file:
-            return file.read()
+        # TODO continue adding code
 
 
 if __name__ == "__main__":
