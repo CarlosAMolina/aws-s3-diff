0fab7752c94b883636c6058623582be05f43a04e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-24 18:16:58 +0100

    test(config): refactor inline variable

--- tests/config.py
+++ tests/config.py
@@ -10,6 +10,5 @@ def get_config_for_the_test() -> Config:
     aws_account = "aws_account_1_pro"
     file_what_to_analyze_path = current_path.joinpath("fake-files", _S3UrisFile._FILE_NAME_S3_URIS)
     result = Config(aws_account, file_what_to_analyze_path)
-    directory_s3_results_path = current_path.joinpath("fake-files", FOLDER_NAME_S3_RESULTS)
-    result._directory_s3_results_path = directory_s3_results_path
+    result._directory_s3_results_path = current_path.joinpath("fake-files", FOLDER_NAME_S3_RESULTS)
     return result
