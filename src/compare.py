e033baa5a03d3978fdd5d005bb5205d8c0384ab9
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-28 00:39:18 +0100

    refactor(compare): inline constant

--- src/compare.py
+++ src/compare.py
@@ -3,20 +3,17 @@ from pandas import DataFrame as Df
 from combine import get_df_combine_files
 from local_results import LocalResults
 
-_AWS_ACCOUNT_WITH_DATA_TO_SYNC_PREFIX = "aws_account_1"
-_AWS_ACCOUNT_WITHOUT_MORE_FILES_PREFIX = "aws_account_2"
-
 
 def _get_aws_account_with_data_to_sync() -> str:
     for aws_account in LocalResults()._get_aws_accounts_analyzed():
-        if aws_account.startswith(_AWS_ACCOUNT_WITH_DATA_TO_SYNC_PREFIX):
+        if aws_account.startswith("aws_account_1"):
             return aws_account
     raise ValueError("No aws account to sync")
 
 
 def _get_aws_account_that_must_not_have_more_files() -> str:
     for aws_account in LocalResults()._get_aws_accounts_analyzed():
-        if aws_account.startswith(_AWS_ACCOUNT_WITHOUT_MORE_FILES_PREFIX):
+        if aws_account.startswith("aws_account_2"):
             return aws_account
     raise ValueError("No aws account that must not have more files")
 
