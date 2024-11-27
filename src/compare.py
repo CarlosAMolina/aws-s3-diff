0b30b20c9f953d6e985028ae40d6bcefc51d6f95
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-28 00:38:05 +0100

    refactor(analysis): move code to other file

--- src/compare.py
+++ src/compare.py
@@ -1,9 +1,24 @@
 from pandas import DataFrame as Df
 
-from analysis import get_aws_account_that_must_not_have_more_files
-from analysis import get_aws_account_with_data_to_sync
 from combine import get_df_combine_files
 from local_results import LocalResults
+
+_AWS_ACCOUNT_WITH_DATA_TO_SYNC_PREFIX = "aws_account_1"
+_AWS_ACCOUNT_WITHOUT_MORE_FILES_PREFIX = "aws_account_2"
+
+
+def get_aws_account_with_data_to_sync() -> str:
+    for aws_account in LocalResults()._get_aws_accounts_analyzed():
+        if aws_account.startswith(_AWS_ACCOUNT_WITH_DATA_TO_SYNC_PREFIX):
+            return aws_account
+    raise ValueError("No aws account to sync")
+
+
+def get_aws_account_that_must_not_have_more_files() -> str:
+    for aws_account in LocalResults()._get_aws_accounts_analyzed():
+        if aws_account.startswith(_AWS_ACCOUNT_WITHOUT_MORE_FILES_PREFIX):
+            return aws_account
+    raise ValueError("No aws account that must not have more files")
 
 
 class S3DataComparator:
