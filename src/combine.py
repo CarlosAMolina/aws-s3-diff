3eb240f39ba42a41f6f5af332e1427ebefd15b09
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-11-30 15:45:27 +0100

    feat(combine): maintain prefix if no file in any account

--- src/combine.py
+++ src/combine.py
@@ -7,16 +7,76 @@ from local_results import LocalResults
 
 
 def get_df_combine_files() -> Df:
+    # todo extract: _get_df_files_of_s3_queries()
+    # result = _get_df_from_uris_to_analyze()
+    # result["name"] = None
+    # for aws_account in LocalResults()._get_aws_accounts_analyzed():
+    #    account_df = _get_df_for_aws_account_only_files(aws_account)
+    #    print(result)
+    #    account_df = account_df[list(S3Query._fields) + ["name"]]
+    #    print(account_df)
+    #    result = pd.concat([result, account_df])
+    #    print(result)
+    #    breakpoint()
+    #    result = result.join(account_df, how="outer")
+    #    print(result)
+    #    breakpoint()
+    #    account_df = account_df.reset_index(level="name")
+    #    #account_df = account_df[["name"]]
+    #    result = result.join(account_df, how="left")
+    #    print(result)
+    #    breakpoint()
+    # breakpoint()
+
+    # todo extract: _get_df_files_of_s3_queries()
+    # aws_accounts = LocalResults()._get_aws_accounts_analyzed()
+    # result = _get_df_for_aws_account_only_files(aws_accounts[0])
+    # result = result[list(S3Query._fields) + ["name"]]
+    # for aws_account in aws_accounts:
+    #    account_df = _get_df_for_aws_account_only_files(aws_account)
+    #    account_df = account_df[list(S3Query._fields) + ["name"]]
+    #    result = pd.concat([result, account_df], ignore_index=True)
+    # result = result.drop_duplicates()
+    # result = result.sort_values(list(S3Query._fields) + ["name"], ignore_index=True)
+    # count_files_per_bucket_and_path_df = result.groupby(['bucket','prefix']).count()
+    # count_files_per_bucket_and_path_df = count_files_per_bucket_and_path_df.rename(columns={"name": "count_files_in_bucket_prefix"})
+    # result = result.set_index(["bucket","prefix"])
+    # result = result.join(count_files_per_bucket_and_path_df, how="left")
+    # result = result.loc[(~result["name"].isna()) | (result["count_files_in_bucket_prefix"] == 0)]
+    # result = result.drop(columns=['count_files_in_bucket_prefix'])
+    # result = result.reset_index().set_index(["bucket", "prefix", "name"])
+    # for aws_account in aws_accounts:
+    #    print(result)
+    #    account_df = _get_df_for_aws_account(aws_account)
+    #    print(account_df)
+    #    breakpoint()
+    #    # TODO rename fuction, incorrect name as more columns are retrieved
+    #    result = result.join(account_df, how="left")
+    #    #result = result.merge(account_df, on=["bucket","prefix","name"], how="outer")
+    # print(result)
+    # breakpoint()
+    # return result
+
     aws_accounts = LocalResults()._get_aws_accounts_analyzed()
     result = _get_df_for_aws_account(aws_accounts[0])
     for aws_account in aws_accounts[1:]:
         account_df = _get_df_for_aws_account(aws_account)
         result = result.join(account_df, how="outer")
-    # TODO not drop only if its the only bucket and prefix, in
-    # TODO order to maintain empty query results. And add this situtation to the tests
-    # TODO: possible query:
-    # TODO result.reset_index(level="name")[["name"]].groupby(["bucket","prefix"]).count() > 0
-    return result.dropna(axis="index", how="all")
+    count_files_per_bucket_and_path_df = (
+        Df(result.index.to_list(), columns=result.index.names).groupby(["bucket", "prefix"]).count()
+    )
+    count_files_per_bucket_and_path_df.columns = pd.MultiIndex.from_tuples(
+        [
+            ("count", "files_in_bucket_prefix"),
+        ]
+    )
+    result = result.join(count_files_per_bucket_and_path_df)
+    result = result.reset_index()
+    # Drop null rows provoced by queries without results in some accounts.
+    # Avoid drop queries without results in any aws account
+    result = result.loc[(~result["name"].isna()) | (result[("count", "files_in_bucket_prefix")] == 0)]
+    result = result.set_index(["bucket", "prefix", "name"])
+    return result.drop(columns=(("count", "files_in_bucket_prefix")))
 
 
 # TODO when reading the uris to check, assert all accounts all paths to analyze.
@@ -46,6 +106,7 @@ def _get_column_names_mult_index(aws_account: str, column_names: list[str]) -> l
 # TODO ```
 
 
+# TODO rename specify _aws_account_results_file
 def _get_df_from_file(file_path_name: Path) -> Df:
     return pd.read_csv(
         file_path_name,
