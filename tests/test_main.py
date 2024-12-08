112c2880d10659686eaef00e71bc45a89797172d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-08 23:40:17 +0100

    test(main): fix use test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
-from unittest import mock
+from unittest.mock import patch
+from unittest.mock import PropertyMock
 
 from moto import mock_aws
 from pandas import DataFrame as Df
@@ -23,11 +24,13 @@ class TestFunction_run(unittest.TestCase):
         if LocalResults()._get_file_path_accounts_analysis_date_time().is_file():
             LocalResults().remove_file_with_analysis_date()
 
-    def tearDown(self):
-        LocalResults().remove_file_with_analysis_date()
-
-    @mock.patch("src.main.input", create=True)
-    def test_run(self, mock_input):
+    @patch(
+        "src.analysis.S3UrisFileReader._directory_path_what_to_analyze",
+        new_callable=PropertyMock,
+        return_value=Path(__file__).parent.absolute().joinpath("fake-files"),
+    )
+    @patch("src.main.input", create=True)
+    def test_run(self, mock_input, mock_directory_path_what_to_analyze):
         mock_input.side_effect = ["Y"] * len(S3UrisFileReader().get_aws_accounts())
         for aws_account in S3UrisFileReader().get_aws_accounts():
             self.mock_aws.start()
