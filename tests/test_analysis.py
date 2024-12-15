294475fbcba5717246f06c75cd7b7efae4d11b11
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-16 00:18:38 +0100

    test(analysis): test target account without more files

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -15,6 +15,7 @@ from src.analysis import _AnalysisDfToCsv
 from src.analysis import _AnalysisGenerator
 from src.analysis import _CompareAwsAccounts
 from src.analysis import _OriginFileSyncDfAnalysis
+from src.analysis import _TargetAccountWithoutMoreFilesDfAnalysis
 from src.s3_uris_to_analyze import S3UrisFileReader
 
 
@@ -98,6 +99,74 @@ class TestOriginFileSyncDfAnalysis(unittest.TestCase):
         result = _OriginFileSyncDfAnalysis(self._aws_accounts_to_compare, df).get_df_set_analysis()
         result_to_check = result.loc[:, ("analysis", "is_sync_ok_in_aws_account_2_release")].tolist()
         expected_result = [False]
+        self.assertEqual(expected_result, result_to_check)
+
+    @property
+    def _aws_accounts_to_compare(self) -> _CompareAwsAccounts:
+        all_aws_accounts = S3UrisFileReader().get_aws_accounts()
+        return _CompareAwsAccounts(*all_aws_accounts[:2])
+
+
+class TestTargetAccountWithoutMoreFilesAnalysisConfig(unittest.TestCase):
+    # TODO refactor extract common patchs.
+    @patch(
+        "src.analysis.LocalResults.get_file_path_s3_data_all_accounts",
+        return_value=Path(__file__)
+        .parent.absolute()
+        .joinpath("fake-files/test-origin-file-sync/s3-files-all-accounts/file-sync-ok.csv"),
+    )
+    @patch(
+        "src.analysis.S3UrisFileReader._directory_path_what_to_analyze",
+        new_callable=PropertyMock,
+        return_value=Path(__file__).parent.absolute().joinpath("fake-files/test-origin-file-sync/"),
+    )
+    def test_get_df_set_analysis_result_if_file_sync(
+        self, mock_directory_path_what_to_analyze, mock_get_file_path_s3_data_all_accounts
+    ):
+        df = get_df_s3_data_all_accounts()
+        result = _TargetAccountWithoutMoreFilesDfAnalysis(self._aws_accounts_to_compare, df).get_df_set_analysis()
+        result_to_check = result.loc[:, ("analysis", "should_exist_in_aws_account_2_release")].tolist()
+        expected_result = [None]
+        self.assertEqual(expected_result, result_to_check)
+
+    @patch(
+        "src.analysis.LocalResults.get_file_path_s3_data_all_accounts",
+        return_value=Path(__file__)
+        .parent.absolute()
+        .joinpath("fake-files/test-origin-file-sync/s3-files-all-accounts/file-not-in-origin.csv"),
+    )
+    @patch(
+        "src.analysis.S3UrisFileReader._directory_path_what_to_analyze",
+        new_callable=PropertyMock,
+        return_value=Path(__file__).parent.absolute().joinpath("fake-files/test-origin-file-sync/"),
+    )
+    def test_get_df_set_analysis_result_if_no_file_to_sync(
+        self, mock_directory_path_what_to_analyze, mock_get_file_path_s3_data_all_accounts
+    ):
+        df = get_df_s3_data_all_accounts()
+        result = _TargetAccountWithoutMoreFilesDfAnalysis(self._aws_accounts_to_compare, df).get_df_set_analysis()
+        result_to_check = result.loc[:, ("analysis", "should_exist_in_aws_account_2_release")].tolist()
+        expected_result = [False]
+        self.assertEqual(expected_result, result_to_check)
+
+    @patch(
+        "src.analysis.LocalResults.get_file_path_s3_data_all_accounts",
+        return_value=Path(__file__)
+        .parent.absolute()
+        .joinpath("fake-files/test-origin-file-sync/s3-files-all-accounts/file-not-in-target.csv"),
+    )
+    @patch(
+        "src.analysis.S3UrisFileReader._directory_path_what_to_analyze",
+        new_callable=PropertyMock,
+        return_value=Path(__file__).parent.absolute().joinpath("fake-files/test-origin-file-sync/"),
+    )
+    def test_get_df_set_analysis_result_if_no_file_in_target_account(
+        self, mock_directory_path_what_to_analyze, mock_get_file_path_s3_data_all_accounts
+    ):
+        df = get_df_s3_data_all_accounts()
+        result = _TargetAccountWithoutMoreFilesDfAnalysis(self._aws_accounts_to_compare, df).get_df_set_analysis()
+        result_to_check = result.loc[:, ("analysis", "should_exist_in_aws_account_2_release")].tolist()
+        expected_result = [None]
         self.assertEqual(expected_result, result_to_check)
 
     @property
