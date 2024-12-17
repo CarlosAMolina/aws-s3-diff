6577d0d8186000d9554806e4ca4ea68fb2327ff1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-17 12:44:23 +0100

    feat(Query): improve how it is printed

--- src/types_custom.py
+++ src/types_custom.py
@@ -1,8 +1,16 @@
-from collections import namedtuple
+from typing import NamedTuple
 
 from pandas import DataFrame as Df
 
-S3Query = namedtuple("S3Query", "bucket prefix")
+
+class S3Query(NamedTuple):
+    bucket: str
+    prefix: str
+
+    def __repr__(self):
+        return f"s3://{self.bucket}/{self.prefix}"
+
+
 _S3FileData = dict
 S3Data = list[_S3FileData]
 AllAccoutsS3DataDf = Df  # It is the combination of all AWS accounts S3 data.
