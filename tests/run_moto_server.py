387abb79a289c363350dbd75946691530e120cab
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-19 19:28:07 +0100

    test(run local server): refactor inline variable

--- tests/run_moto_server.py
+++ tests/run_moto_server.py
@@ -5,10 +5,9 @@ from moto.server import ThreadedMotoServer
 
 server = ThreadedMotoServer()
 server.start()
-aws_account = "aws_account_1_pro"
 ENDPOINT_URL = "http://localhost:5000"
 print("MotoServer:", ENDPOINT_URL)
-S3(aws_account, ENDPOINT_URL).create_objects()
+S3("aws_account_1_pro", ENDPOINT_URL).create_objects()
 print("You can now run aws commands in another terminal. Example: aws --endpoint-url http://localhost:5000 s3 ls")
 input("Press enter to stop the MotoServer")
 print("Stopping MotoServer")
