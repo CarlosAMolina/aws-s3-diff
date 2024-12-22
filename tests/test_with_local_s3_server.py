0036a614dcc2a65ad183166a7e3ffd0e7472fb5e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-22 14:56:19 +0100

    test(using local s3 server): refactor change imports

--- tests/test_with_local_s3_server.py
+++ tests/test_with_local_s3_server.py
@@ -1,12 +1,13 @@
 import unittest
 from pathlib import Path
-from unittest import mock
+from unittest.mock import patch
+from unittest.mock import PropertyMock
 
 from src.local_results import _MainPaths
 from src.local_results import LocalResults
+from tests import test_s3_client as m_test_s3_client
+from tests import test_s3_data as m_test_s3_data
 from tests.aws import S3Server
-from tests.test_s3_client import TestS3Client
-from tests.test_s3_data import TestAwsAccountExtractor
 
 ExpectedResult = list[dict]
 
@@ -14,6 +15,7 @@ ExpectedResult = list[dict]
 class TestWithLocalS3Server(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        # TODO rename in all files _s3_server to _mock_s3_server
         cls._s3_server = S3Server()
         cls._s3_server.start()
         # Drop file created by the user or by other tests.
@@ -24,18 +26,18 @@ class TestWithLocalS3Server(unittest.TestCase):
     def tearDownClass(cls):
         cls._s3_server.stop()
 
-    @mock.patch(
+    @patch(
         "src.s3_uris_to_analyze.S3UrisFileAnalyzer._directory_path_what_to_analyze",
-        new_callable=mock.PropertyMock,
+        new_callable=PropertyMock,
         return_value=Path(__file__).parent.absolute().joinpath("fake-files"),
     )
-    def test_run_test_of_TestAwsAccountExtractor(self, mock_directory_path_what_to_analyze):
-        TestAwsAccountExtractor().run_test_extract_generates_expected_result(
+    def test_run_test_of_test_s3_data(self, mock_directory_path_what_to_analyze):
+        m_test_s3_data.TestAwsAccountExtractor().run_test_extract_generates_expected_result(
             mock_directory_path_what_to_analyze, self._s3_server
         )
 
-    def test_run_test_of_TestS3Client(self):
+    def test_run_test_of_m_test_s3_client(self):
         # TODO remove previous created objects?
         self._s3_server.create_objects("aws_account_1_pro")
-        TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
-        TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_pets()
+        m_test_s3_client.TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
+        m_test_s3_client.TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_pets()
