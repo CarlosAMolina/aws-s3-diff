00fc1628092e05aed758c495bc6c2c02690c271f
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-22 19:48:27 +0100

    test(makefile): rename functions and change filtered test

--- tests/test_with_local_s3_server.py
+++ tests/test_with_local_s3_server.py
@@ -37,14 +37,14 @@ class TestWithLocalS3Server(unittest.TestCase):
         new_callable=PropertyMock,
         return_value=f'{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}-test-s3-data',
     )
-    def test_run_test_of_test_s3_data(
+    def test_run_test_s3_data(
         self,
         mock_new_date_time_str,
         mock_directory_path_what_to_analyze,
     ):
         m_test_s3_data.TestAwsAccountExtractor().run_test_extract_generates_expected_result(self._s3_server)
 
-    def test_run_test_of_m_test_s3_client(self):
+    def test_run_test_s3_client(self):
         # TODO remove previous created objects?
         self._s3_server.create_objects("aws_account_1_pro")
         m_test_s3_client.TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
