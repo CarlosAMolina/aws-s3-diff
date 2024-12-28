598cd082f5d3f34b1bb8fc4d4e9ae768a879459a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-28 03:01:47 +0100

    test(local s3 server): change imports

--- tests/test_with_local_s3_server.py
+++ tests/test_with_local_s3_server.py
@@ -5,10 +5,10 @@ from unittest.mock import PropertyMock
 
 from src.local_results import _MainPaths
 from src.local_results import LocalResults
-from tests import test_main as m_test_main
-from tests import test_s3_client as m_test_s3_client
-from tests import test_s3_data as m_test_s3_data
 from tests.aws import S3Server
+from tests.test_main import TestFunction_run
+from tests.test_s3_client import TestS3Client
+from tests.test_s3_data import TestAwsAccountExtractor
 
 ExpectedResult = list[dict]
 
@@ -40,12 +40,12 @@ class TestWithLocalS3Server(unittest.TestCase):
         fake_analysis_date_time_str = "20240101000000-test-s3-data"
         mock_get_analysis_date_time_str.return_value = fake_analysis_date_time_str
         mock_new_date_time_str.return_value = fake_analysis_date_time_str
-        m_test_s3_data.TestAwsAccountExtractor().run_test_extract_generates_expected_result(self._local_s3_server)
+        TestAwsAccountExtractor().run_test_extract_generates_expected_result(self._local_s3_server)
 
     def test_run_test_s3_client(self):
         self._local_s3_server.create_objects("aws_account_1_pro")
-        m_test_s3_client.TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
-        m_test_s3_client.TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_pets()
+        TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
+        TestS3Client().run_test_get_s3_data_returns_expected_result_for_bucket_pets()
 
     @patch(
         "src.main.S3UrisFileAnalyzer._directory_path_what_to_analyze",
@@ -54,6 +54,4 @@ class TestWithLocalS3Server(unittest.TestCase):
     )
     @patch("src.main.input", create=True)
     def test_run(self, mock_input, mock_directory_path_what_to_analyze):
-        m_test_main.TestFunction_run().run_test_run(
-            mock_input, mock_directory_path_what_to_analyze, self._local_s3_server
-        )
+        TestFunction_run().run_test_run(mock_input, mock_directory_path_what_to_analyze, self._local_s3_server)
