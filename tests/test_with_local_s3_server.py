3ea7b8bf1be7e1a733dc13e1d4e5282035f34180
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-12-29 18:51:04 +0100

    test(s3 client): drop local server tests as they are done in test_main.py and requires time

--- tests/test_with_local_s3_server.py
+++ tests/test_with_local_s3_server.py
@@ -7,7 +7,6 @@ from src.local_results import _MainPaths
 from src.local_results import LocalResults
 from tests.aws import S3Server
 from tests.test_main import TestFunction_runLocalS3Server
-from tests.test_s3_client import TestS3ClientLocalS3Server
 
 ExpectedResult = list[dict]
 
@@ -22,11 +21,6 @@ class TestWithLocalS3Server(unittest.TestCase):
 
     def tearDown(self):
         self._local_s3_server.stop()
-
-    def test_run_test_s3_client(self):
-        self._local_s3_server.create_objects("aws_account_1_pro")
-        TestS3ClientLocalS3Server().run_test_get_s3_data_returns_expected_result_for_bucket_cars()
-        TestS3ClientLocalS3Server().run_test_get_s3_data_returns_expected_result_for_bucket_pets()
 
     @patch(
         "src.main.S3UrisFileAnalyzer._directory_path_what_to_analyze",
