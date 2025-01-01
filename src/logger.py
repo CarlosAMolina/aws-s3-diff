e97217c6464b53cc3d0fe6c31bba8c5adb6fecf0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-01 19:52:13 +0100

    fix(logger): use module name

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 
 
 def get_logger() -> logging.Logger:
-    logger = logging.getLogger("foo")
+    logger = logging.getLogger(__name__)
     logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
     # Avoid other modules logs: https://stackoverflow.com/a/49814387
     logging.getLogger("botocore").setLevel(logging.ERROR)
