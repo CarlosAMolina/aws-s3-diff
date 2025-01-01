1970fe1713347ec9422cba54c5a83405a3670e14
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-01 19:56:47 +0100

    feat(logger): avoid some loggers logs

--- src/logger.py
+++ src/logger.py
@@ -5,5 +5,6 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger(__name__)
     logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
     # Avoid other modules logs: https://stackoverflow.com/a/49814387
-    logging.getLogger("botocore").setLevel(logging.ERROR)
+    for logger_name in ("boto3", "botocore", "s3transfer"):
+        logging.getLogger(logger_name).setLevel(logging.ERROR)
     return logger
