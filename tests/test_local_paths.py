3bbd0260e2575d4cd731925d6556ad18d19ad46c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-20 21:16:29 +0100

    test(local_paths): add tests

--- tests/test_local_paths.py
+++ tests/test_local_paths.py
@@ -11,3 +11,12 @@ class TestLocalPaths(unittest.TestCase):
 
     def test_config_directory_has_expected_value(self):
         self.assertEqual(self._current_path.parent.joinpath("config"), LocalPaths().config_directory)
+
+    def test_all_results_directory_has_expected_value(self):
+        self.assertEqual(self._current_path.parent.joinpath("s3-results"), LocalPaths().all_results_directory)
+
+    def test_analysis_date_time_file_has_expected_value(self):
+        self.assertEqual(
+            self._current_path.parent.joinpath("s3-results/analysis_date_time.txt"),
+            LocalPaths().analysis_date_time_file,
+        )
