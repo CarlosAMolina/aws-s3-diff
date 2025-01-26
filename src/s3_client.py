9eb0bfcde30e660d0096d1c72433c0db221f849b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-26 12:58:40 +0100

    refactor(s3 client): extract class and move lines

--- src/s3_client.py
+++ src/s3_client.py
@@ -13,44 +13,16 @@ from types_custom import S3Query
 class S3Client:
     def __init__(self):
         self._s3_requester = _S3Requester()
+        self._response_analyzer = _ResponseAnalyzer()
 
     def get_s3_data(self, s3_query: S3Query) -> Iterator[S3Data]:
         """https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_objects_v2.html"""
         response = self._s3_requester.get_response(s3_query)
         while response["KeyCount"] != 0:
-            self._raise_exception_if_folders_in_response(response, s3_query.bucket)
-            yield self._get_s3_data_from_response(response)
+            self._response_analyzer.raise_exception_if_folders_in_response(response, s3_query.bucket)
+            yield self._response_analyzer.get_s3_data_from_response(response)
             last_key = response["Contents"][-1]["Key"]
             response = self._s3_requester.get_response(s3_query, last_key)
-
-    def _raise_exception_if_folders_in_response(self, response: dict, bucket: str):
-        folder_path_names = self._get_folder_path_names_in_response_list_objects_v2(response)
-        if len(folder_path_names) == 0:
-            return
-        folder_path_names = [common_prefix["Prefix"] for common_prefix in response["CommonPrefixes"]]
-        error_text = (
-            f"Subfolders detected in bucket '{bucket}'. The current version of the program cannot manage subfolders"
-            f". Subfolders ({len(folder_path_names)}): {', '.join(folder_path_names)}"
-        )
-        raise FolderInS3UriError(error_text)
-
-    def _get_folder_path_names_in_response_list_objects_v2(self, response: dict) -> list[str]:
-        # Detect folders: https://stackoverflow.com/a/71579041
-        if "CommonPrefixes" not in response:
-            return []
-        return [common_prefix["Prefix"] for common_prefix in response["CommonPrefixes"]]
-
-    def _get_s3_data_from_response(self, response: dict) -> S3Data:
-        return [self._get_file_s3_data_from_s3_response_content(content) for content in response["Contents"]]
-
-    @staticmethod
-    def _get_file_s3_data_from_s3_response_content(s3_response_content: dict) -> FileS3Data:
-        return FileS3Data(
-            Path(s3_response_content["Key"]).name,
-            s3_response_content["LastModified"],
-            s3_response_content["Size"],
-            s3_response_content["ETag"].strip('"'),
-        )
 
 
 class _S3Requester:
@@ -71,3 +43,34 @@ class _S3Requester:
         if last_key:
             result["StartAfter"] = last_key
         return result
+
+
+class _ResponseAnalyzer:
+    def raise_exception_if_folders_in_response(self, response: dict, bucket: str):
+        folder_path_names = self._get_folder_path_names_in_response_list_objects_v2(response)
+        if len(folder_path_names) == 0:
+            return
+        folder_path_names = [common_prefix["Prefix"] for common_prefix in response["CommonPrefixes"]]
+        error_text = (
+            f"Subfolders detected in bucket '{bucket}'. The current version of the program cannot manage subfolders"
+            f". Subfolders ({len(folder_path_names)}): {', '.join(folder_path_names)}"
+        )
+        raise FolderInS3UriError(error_text)
+
+    def get_s3_data_from_response(self, response: dict) -> S3Data:
+        return [self._get_file_s3_data_from_s3_response_content(content) for content in response["Contents"]]
+
+    def _get_folder_path_names_in_response_list_objects_v2(self, response: dict) -> list[str]:
+        # Detect folders: https://stackoverflow.com/a/71579041
+        if "CommonPrefixes" not in response:
+            return []
+        return [common_prefix["Prefix"] for common_prefix in response["CommonPrefixes"]]
+
+    @staticmethod
+    def _get_file_s3_data_from_s3_response_content(s3_response_content: dict) -> FileS3Data:
+        return FileS3Data(
+            Path(s3_response_content["Key"]).name,
+            s3_response_content["LastModified"],
+            s3_response_content["Size"],
+            s3_response_content["ETag"].strip('"'),
+        )
