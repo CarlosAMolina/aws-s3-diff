fb2c12276231041f8aefc6198bece1b6557d6b4e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-02-02 22:23:49 +0100

    test(analysis): refactor inline variable

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -151,9 +151,8 @@ class TestAnalysisS3DataFactory(unittest.TestCase):
 
 def _get_df_combine_accounts_s3_data_csv(file_path_name: str) -> Df:
     s3_data_csv_to_df = _CombinedAccountsS3DataCsvToDf()
-    mock_local_results = Mock()
-    mock_local_results.analysis_paths.file_s3_data_all_accounts = (
+    s3_data_csv_to_df._local_results = Mock()
+    s3_data_csv_to_df._local_results.analysis_paths.file_s3_data_all_accounts = (
         Path(__file__).parent.absolute().joinpath(file_path_name)
     )
-    s3_data_csv_to_df._local_results = mock_local_results
     return s3_data_csv_to_df.get_df()
