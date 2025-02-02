c0199b798ca0f715d192d8f1b33a99e4c45ea6f0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-02-02 20:26:52 +0100

    refactor(s3_data): move one account code to its own file

--- src/s3_data.py
+++ src/s3_data.py
@@ -1,22 +1,16 @@
 import re
 from pathlib import Path
 
-import pandas as pd
 from pandas import DataFrame as Df
 from pandas import Index
 from pandas import MultiIndex
 from pandas import read_csv
 
-from config_files import REGEX_BUCKET_PREFIX_FROM_S3_URI
+import one_account_s3_data
 from config_files import S3UrisFileReader
 from local_results import LocalResults
 from logger import get_logger
-from s3_client import S3Client
-from types_custom import AccountS3DataDf
 from types_custom import AllAccountsS3DataDf
-from types_custom import FileS3Data
-from types_custom import S3Data
-from types_custom import S3Query
 
 
 class AllAccountsS3DataFactory:
@@ -35,62 +29,14 @@ class AllAccountsS3DataFactory:
         return _CombinedAccountsS3DataCsvToDf().get_df(file_path)
 
 
-class AccountS3DataFactory:
-    def __init__(self, account: str):
-        self._account = account
-        self._local_results = LocalResults()
-        self._s3_uris_file_reader = S3UrisFileReader()
-
-    def to_csv_extract_s3_data(self):
-        _AccountExtractor(
-            self._local_results.get_file_path_account_results(self._account),
-            self._s3_uris_file_reader.get_s3_queries_for_account(self._account),
-        ).extract()
-
-    def get_df_from_csv(self) -> Df:
-        return _AccountS3DataDfBuilder(self._account).with_multi_index().build()
-
-    def get_df_from_csv_with_original_account_index(self) -> Df:
-        return _AccountS3DataDfBuilder(self._account).with_multi_index().with_origin_account_index().build()
+# TODO deprecate
+class AccountS3DataFactory(one_account_s3_data.AccountS3DataFactory):
+    pass
 
 
-class _AccountExtractor:
-    def __init__(self, file_path_results: Path, s3_queries: list[S3Query]):
-        self._file_path_results = file_path_results
-        self._logger = get_logger()
-        self._s3_queries = s3_queries
-
-    def extract(self):
-        self._logger.info(f"Exporting AWS account information to {self._file_path_results}")
-        for query_index, s3_query in enumerate(self._s3_queries, 1):
-            self._logger.info(f"Analyzing S3 URI {query_index}/{len(self._s3_queries)}: {s3_query}")
-            try:
-                self._extract_s3_data_of_query(s3_query)
-            except Exception as exception:
-                self._drop_file()
-                raise exception
-
-    def _extract_s3_data_of_query(self, s3_query: S3Query):
-        is_any_result = False
-        for s3_data in S3Client(s3_query).get_s3_data():
-            is_any_result = True
-            self._export_s3_data_to_csv(s3_data, s3_query)
-        if not is_any_result:
-            self._export_s3_data_to_csv([FileS3Data()], s3_query)
-
-    def _export_s3_data_to_csv(self, s3_data: S3Data, s3_query: S3Query):
-        query_and_data_df = self._get_df_query_and_data(s3_data, s3_query)
-        export_headers = not self._file_path_results.is_file()
-        query_and_data_df.to_csv(header=export_headers, index=False, mode="a", path_or_buf=self._file_path_results)
-
-    def _get_df_query_and_data(self, s3_data: S3Data, s3_query: S3Query) -> Df:
-        result = Df(file_data._asdict() for file_data in s3_data)
-        result.insert(0, "bucket", s3_query.bucket)
-        result.insert(1, "prefix", s3_query.prefix)
-        return result
-
-    def _drop_file(self):
-        self._file_path_results.unlink()
+# TODO deprecate
+class _AccountExtractor(one_account_s3_data._AccountExtractor):
+    pass
 
 
 class _CombinedAccountsS3DataDfToCsv:
@@ -196,95 +142,11 @@ class _AccountsS3DataDfCombinator:
         return result.drop(columns=(("count", "files_in_bucket_prefix")))
 
 
-class _AccountS3DataDfBuilder:
-    def __init__(self, account: str):
-        self._account = account
-        self._local_results = LocalResults()
-        self._s3_uris_file_reader = S3UrisFileReader()
-        self.__df = None  # To avoid read file more than once.
-
-    def with_multi_index(self) -> "_AccountS3DataDfBuilder":
-        self._df.columns = MultiIndex.from_tuples(self._column_names_mult_index)
-        return self
-
-    def with_origin_account_index(self) -> "_AccountS3DataDfBuilder":
-        """The with_multi_index method must be called before this method is called"""
-        self._df = _S3UriDfModifier(
-            self._s3_uris_file_reader.get_first_account(), self._account, self._df
-        ).get_df_set_s3_uris_in_origin_account()
-        return self
-
-    def build(self) -> AccountS3DataDf:
-        return self._df
-
-    @property
-    def _df(self) -> Df:
-        if self.__df is None:
-            local_file_path_name = self._local_results.get_file_path_account_results(self._account)
-            self.__df = self._get_df_account_from_file(local_file_path_name)
-        return self.__df
-
-    def _get_df_account_from_file(self, file_path_name: Path) -> Df:
-        return pd.read_csv(
-            file_path_name,
-            index_col=["bucket", "prefix", "name"],
-            parse_dates=["date"],
-        ).astype({"size": "Int64"})
-
-    @_df.setter
-    def _df(self, df: Df):
-        self.__df = df
-
-    @property
-    def _column_names_mult_index(self) -> list[tuple[str, str]]:
-        return [(self._account, column_name) for column_name in self._df.columns]
+# TODO deprecate
+class _AccountS3DataDfBuilder(one_account_s3_data._AccountS3DataDfBuilder):
+    pass
 
 
-class _S3UriDfModifier:
-    def __init__(self, *args):
-        self._account_origin, self._account_target, self._df = args
-        self._s3_uris_file_reader = S3UrisFileReader()
-
-    def get_df_set_s3_uris_in_origin_account(self) -> Df:
-        s3_uris_map_df = self._s3_uris_file_reader.get_df_s3_uris_map_between_accounts(
-            self._account_origin, self._account_target
-        )
-        if s3_uris_map_df[self._account_origin].equals(s3_uris_map_df[self._account_target]):
-            return self._df
-        return self._get_df_replace_index_with_s3_uris_map(s3_uris_map_df)
-
-    def _get_df_replace_index_with_s3_uris_map(self, s3_uris_map_df: Df) -> Df:
-        original_length = len(self._df)
-        assert self._df.index.get_level_values("prefix").str.endswith("/").all()
-        result = self._df.copy()
-        result = result.reset_index("name")
-        s3_uris_map_df = self._get_s3_uris_map_prepared_for_join(s3_uris_map_df)
-        result = result.join(s3_uris_map_df)
-        if True in result[[f"{self._account_origin}_bucket", f"{self._account_origin}_prefix"]].isna().any().values:
-            raise ValueError("Some values could not be replaced")
-        result = result.rename(
-            columns={f"{self._account_origin}_bucket": "bucket", f"{self._account_origin}_prefix": "prefix"}
-        )
-        result = result.reset_index(drop=True).set_index(["bucket", "prefix", "name"])
-        assert original_length == len(result)
-        return result
-
-    def _get_s3_uris_map_prepared_for_join(self, s3_uris_map_df: Df) -> Df:
-        result = s3_uris_map_df.copy()
-        for account in (self._account_origin, self._account_target):
-            result[[f"{account}_bucket", f"{account}_prefix"]] = result[account].str.extract(
-                REGEX_BUCKET_PREFIX_FROM_S3_URI, expand=False
-            )
-            result.loc[~result[f"{account}_prefix"].str.endswith("/"), f"{account}_prefix"] = (
-                result[f"{account}_prefix"] + "/"
-            )
-        result.drop(columns=[f"{account}" for account in (self._account_origin, self._account_target)], inplace=True)
-        result = result.rename(
-            columns={f"{self._account_target}_bucket": "bucket", f"{self._account_target}_prefix": "prefix"}
-        )
-        result = result.set_index(["bucket", "prefix"])
-        result.columns = [
-            result.columns,
-            [""] * len(result.columns),
-        ]  # To merge to a MultiIndex columns Df.
-        return result
+# TODO deprecate
+class _S3UriDfModifier(one_account_s3_data._S3UriDfModifier):
+    pass
