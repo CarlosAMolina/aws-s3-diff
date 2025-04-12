b77b53c298f0556d8fa993b8c1cad7b08bb4c4a4
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-04-12 02:44:44 +0200

    refactor(logger): as global variable instead of attribute

--- src/main.py
+++ src/main.py
@@ -15,10 +15,11 @@ from s3_data.all_accounts import AccountsDf
 from s3_data.analysis import AnalysisCsvCreator
 from s3_data.one_account import AccountCsvCreator
 
+_logger = get_logger()
+
 
 class _Main:
     def __init__(self):
-        self._logger = get_logger()
         self._process_creator = _ProcessCreator()
         self._s3_uris_file_reader = S3UrisFileReader()
 
@@ -26,25 +27,25 @@ class _Main:
         try:
             self._run_without_catching_exceptions()
         except (AnalysisConfigError, FolderInS3UriError) as exception:
-            self._logger.error(exception)
+            _logger.error(exception)
             return
         # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
         except ClientError as exception:
             error_code = exception.response["Error"]["Code"]
             if error_code == "NoSuchBucket":
                 bucket_name = exception.response["Error"]["BucketName"]
-                self._logger.error(
+                _logger.error(
                     f"The bucket '{bucket_name}' does not exist. Specify a correct bucket and run the program again"
                 )
                 return
             if error_code in ("AccessDenied", "InvalidAccessKeyId"):
-                self._logger.error("Incorrect AWS credentials. Authenticate and run the program again")
+                _logger.error("Incorrect AWS credentials. Authenticate and run the program again")
                 return
             raise Exception from exception
 
     def _run_without_catching_exceptions(self):
-        self._logger.info("Welcome to the AWS S3 Diff tool!")
-        self._logger.debug("Checking if the URIs to analyze configuration file is correct")
+        _logger.info("Welcome to the AWS S3 Diff tool!")
+        _logger.debug("Checking if the URIs to analyze configuration file is correct")
         S3UrisFileChecker().assert_file_is_correct()
         self._show_accounts_to_analyze()
         self._process_creator.get_process().run()
@@ -52,7 +53,7 @@ class _Main:
     def _show_accounts_to_analyze(self):
         accounts = self._s3_uris_file_reader.get_accounts()
         accounts_list = [f"\n{index}. {account}" for index, account in enumerate(accounts, 1)]
-        self._logger.info(f"AWS accounts configured to be analyzed:{''.join(accounts_list)}")
+        _logger.info(f"AWS accounts configured to be analyzed:{''.join(accounts_list)}")
 
 
 class _Process(ABC):
@@ -108,10 +109,9 @@ class _AnalyzedAccounts:
 class _AccountProcess(_Process):
     def __init__(self):
         self._analyzed_accounts = _AnalyzedAccounts()
-        self._logger = get_logger()  # TODO? as global variable
 
     def run(self):
-        self._logger.info(f"Analyzing the AWS account '{self._account}'")
+        _logger.info(f"Analyzing the AWS account '{self._account}'")
         # TODO initialize class in __init__
         AccountCsvCreator(self._account).export_csv()
 
@@ -135,16 +135,12 @@ class _AccountProcessCreator:
 
 
 class _NoLastAccountProcess(_AccountProcess):
-    def __init__(self):
-        self._analyzed_accounts = _AnalyzedAccounts()
-        super().__init__()
-
     def run(self):
         super().run()
         self._show_next_account_to_analyze()
 
     def _show_next_account_to_analyze(self):
-        self._logger.info(
+        _logger.info(
             f"The next account to be analyzed is '{self._analyzed_accounts.get_account_to_analyze()}'"
             ". Authenticate and run the program again"
         )
@@ -187,7 +183,6 @@ class _NoCombinedS3DataProcess(_Process):
 
 class _AnalysisProcess(_Process):
     def __init__(self):
-        self._logger = get_logger()
         self._analysis_config_reader = AnalysisConfigReader()
         self._analysis_config_checker = AnalysisConfigChecker()
         self._analysis_csv_creator = AnalysisCsvCreator()
@@ -197,7 +192,7 @@ class _AnalysisProcess(_Process):
             self._analysis_config_checker.assert_file_is_correct()
             self._analysis_csv_creator.export_csv()
         else:
-            self._logger.info("No analysis configured. Omitting")
+            _logger.info("No analysis configured. Omitting")
         LocalResults().drop_file_with_analysis_date()
 
 
