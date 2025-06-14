8ccc9d26b1e70caaffbe8f25ea47bbc0d0880926
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-06-15 01:31:55 +0200

    refactor(analyzed account): simplify logic to get account to analyze

--- aws_s3_diff/accounts.py
+++ aws_s3_diff/accounts.py
@@ -9,14 +9,11 @@ class AnalyzedAccounts:
 
     # TODO replace in all files the account variable with a call to this method
     def get_account_to_analyze(self) -> str:
-        accounts_to_analyze = self._s3_uris_file_reader.get_accounts()
-        last_account_analyzed = self._get_last_account_analyzed()
-        if last_account_analyzed is None:
-            return self._s3_uris_file_reader.get_first_account()
-        if last_account_analyzed == self._s3_uris_file_reader.get_last_account():
-            # Unexpected situation. This method cannot be called if all accounts have been analyzed.
-            raise RuntimeError("All AWS accounts have been analyzed")
-        return accounts_to_analyze[accounts_to_analyze.index(last_account_analyzed) + 1]
+        for account_to_analyze in self._s3_uris_file_reader.get_accounts():
+            if not self._local_results.has_this_account_been_analyzed(account_to_analyze):
+                return account_to_analyze
+        # Unexpected situation. This method cannot be called if all accounts have been analyzed.
+        raise RuntimeError("All AWS accounts have been analyzed")
 
     def have_all_accounts_been_analyzed(self) -> bool:
         return self._get_last_account_analyzed() == self._s3_uris_file_reader.get_last_account()
