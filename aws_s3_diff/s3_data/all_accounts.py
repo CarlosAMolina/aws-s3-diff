6a58e2fb9e201ce0acf5840fee24f489e0a4365b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-06-21 20:21:50 +0200

    refactor(account): extract variable and not add to array

--- aws_s3_diff/s3_data/all_accounts.py
+++ aws_s3_diff/s3_data/all_accounts.py
@@ -91,12 +91,13 @@ class AccountsDataGenerator(DataGenerator):
     def _get_df_merge_accounts_s3_data(self) -> Df:
         accounts = self._s3_uris_file_reader.get_accounts()
         account_origin = accounts[0]
-        target_account_df_array = [AccountCsvReader(account_origin).get_df()]
+        account_origin_df = AccountCsvReader(account_origin).get_df()
+        target_account_df_array = []
         for account in accounts[1:]:
             account_df = AccountCsvReader(account).get_df()
             account_df_to_join = AccountDf(account, account_df, account_origin).get_account_df_to_join()
             target_account_df_array.append(account_df_to_join)
-        result = target_account_df_array[0].join(target_account_df_array[1:], how="outer")
+        result = account_origin_df.join(target_account_df_array, how="outer")
         return result.dropna(axis="index", how="all")
 
     def _get_df_set_all_queries_despite_without_results(self, df: Df) -> Df:
