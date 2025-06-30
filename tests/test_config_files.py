18ed1b6afbfd74192a9079aaf793ecd4a6c688d4
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-06-30 21:21:07 +0200

    refactor(config files): change mocked oject

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -115,16 +115,24 @@ class TestS3UrisFileChecker(unittest.TestCase):
             m_config_files.S3UrisFileChecker().assert_file_is_correct()
         self.assertEqual("Error in s3-uris-to-analyze.csv. Some AWS account names are empty", str(exception.exception))
 
-    @mock.patch("aws_s3_diff.config_files.S3UrisFileReader._file_path_what_to_analyze", new_callable=mock.PropertyMock)
-    def test_assert_file_is_correct_raises_exception_if_empty_uri(self, mock_file_path_what_to_analyze):
-        mock_file_path_what_to_analyze.return_value = self._get_file_path_s3_uri_to_analyze("empty_uri.csv")
+    @mock.patch(
+        "aws_s3_diff.config_files.LocalPaths.config_directory", new_callable=mock.PropertyMock, return_value=mock.Mock()
+    )
+    def test_assert_file_is_correct_raises_exception_if_empty_uri(self, mock_config_directory):
+        mock_config_directory.return_value.joinpath.return_value = self._get_file_path_s3_uri_to_analyze(
+            "empty_uri.csv"
+        )
         with self.assertRaises(EmptyUriS3UrisFileError) as exception:
             m_config_files.S3UrisFileChecker().assert_file_is_correct()
         self.assertEqual("Error in s3-uris-to-analyze.csv. Some URIs are empty", str(exception.exception))
 
-    @mock.patch("aws_s3_diff.config_files.S3UrisFileReader._file_path_what_to_analyze", new_callable=mock.PropertyMock)
-    def test_assert_file_is_correct_raises_exception_if_duplicated_account(self, mock_file_path_what_to_analyze):
-        mock_file_path_what_to_analyze.return_value = self._get_file_path_s3_uri_to_analyze("duplicated_uri.csv")
+    @mock.patch(
+        "aws_s3_diff.config_files.LocalPaths.config_directory", new_callable=mock.PropertyMock, return_value=mock.Mock()
+    )
+    def test_assert_file_is_correct_raises_exception_if_duplicated_account(self, mock_config_directory):
+        mock_config_directory.return_value.joinpath.return_value = self._get_file_path_s3_uri_to_analyze(
+            "duplicated_uri.csv"
+        )
         with self.assertRaises(DuplicatedUriS3UrisFileError) as exception:
             m_config_files.S3UrisFileChecker().assert_file_is_correct()
         self.assertEqual(
