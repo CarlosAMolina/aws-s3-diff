34bc40e72b5fe9ddab41eca602ebddf98aa430b2
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-07-01 21:35:15 +0200

    refactor(config files): replace property with method

--- aws_s3_diff/s3_data/all_accounts.py
+++ aws_s3_diff/s3_data/all_accounts.py
@@ -103,7 +103,7 @@ class AccountsDataGenerator(DataGenerator):
         return result.set_index("name", append=True)
 
     def _get_empty_df_original_account_queries_as_index(self) -> Df:
-        result = self._s3_uris_file_reader.file_df[self._account_origin]
+        result = self._s3_uris_file_reader.get_file_df()[self._account_origin]
         # TODO refactor extract function, this line is done in other files.
         result = result.str.extract(REGEX_BUCKET_PREFIX_FROM_S3_URI, expand=False)
         result.columns = ["bucket", "prefix"]
