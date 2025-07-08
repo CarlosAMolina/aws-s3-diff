f73c11e8d48e036a15c0e7843ed4c1a7748a4b69
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-07-08 21:25:44 +0200

    fix(local s3 server): fix create s3 objects and move code to name main

--- tests/run_local_s3_server.py
+++ tests/run_local_s3_server.py
@@ -4,13 +4,16 @@ from aws import S3
 from aws import set_aws_credentials
 from moto.server import ThreadedMotoServer
 
-set_aws_credentials()
-server = ThreadedMotoServer()
-server.start()
-ENDPOINT_URL = "http://localhost:5000"
-print("MotoServer:", ENDPOINT_URL)
-S3("pro", ENDPOINT_URL).create_objects()
-print("You can now run aws commands in another terminal. Example: aws --endpoint-url http://localhost:5000 s3 ls")
-input("Press enter to stop the MotoServer")
-print("Stopping MotoServer")
-server.stop()
+if __name__ == "__main__":
+    set_aws_credentials()
+    server = ThreadedMotoServer()
+    server.start()
+    ENDPOINT_URL = "http://localhost:5000"
+    print("MotoServer:", ENDPOINT_URL)
+    s3 = S3("pro", ENDPOINT_URL)
+    s3.create_buckets()
+    s3.upload_files()
+    print("You can now run aws commands in another terminal. Example: aws --endpoint-url http://localhost:5000 s3 ls")
+    input("Press enter to stop the MotoServer")
+    print("Stopping MotoServer")
+    server.stop()
