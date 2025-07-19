236cc7ef34eaa7ffa6438a28ac167a48bd5631b0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-07-19 12:50:38 +0200

    docs(df_util): add typing

--- aws_s3_diff/s3_data/df_util.py
+++ aws_s3_diff/s3_data/df_util.py
@@ -1,4 +1,4 @@
-def get_column_name_from_column_multi_index(column_multi_index: tuple[str, str]):
+def get_column_name_from_column_multi_index(column_multi_index: tuple[str, str]) -> str:
     if column_multi_index[1] in ("date", "hash", "size"):
         return f"{column_multi_index[1]}_in_{column_multi_index[0]}"
     return "_".join(column_multi_index)
